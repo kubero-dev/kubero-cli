cbf702af8f29087c7a513ba203992017b7167118
Gianni Carafa <gianni.carafa@srf.ch>
2022-11-02 21:45:11 +0100

    implemente pipeline delete

--- cmd/pipelinesDelete.go
+++ cmd/pipelinesDelete.go
@@ -21,23 +21,15 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("delete called")
+
+		pipeline, _ := client.Delete("/api/cli/pipelines/" + pipeline)
+		fmt.Println(pipeline)
 	},
 }
 
 func init() {
 	pipelinesCmd.AddCommand(pipelinesDeleteCmd)
-	pipelinesDeleteCmd.Flags().StringP("pipeline", "p", "", "Name of the pipeline")
-	//pipelinesDeleteCmd.MarkPersistentFlagRequired("pipeline")
+	pipelinesDeleteCmd.Flags().StringVarP(&pipeline, "pipeline", "p", "", "Name of the pipeline")
+
 	pipelinesDeleteCmd.MarkFlagRequired("pipeline")
-	//pipelinesCmd.MarkFlagRequired("pipeline")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
