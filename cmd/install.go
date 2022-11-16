82dadb2f0c4ac57d5f8af485af629ce9831c3f5e
Gianni Carafa <gianni.carafa@srf.ch>
2022-11-16 11:33:35 +0100

    add OLM and Ingress installer

--- cmd/install.go
+++ cmd/install.go
@@ -4,8 +4,10 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/go-resty/resty/v2"
@@ -22,21 +24,16 @@ var installCmd = &cobra.Command{
 
 required binaries:
  - kubectl
- - operator-sdk
  - kind (optional)`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		checkAllBinaries()
 
-		kind := promptLine("Start a local kubernetes kind cluster", "[y,n]", "n")
-		if kind == "y" {
-			installKind()
-		}
+		installKind()
+		checkCluster()
+		installOLM()
+		installIngress()
 		/*
-			olm := promptLine("Install OLM", "[y,n]", "y")
-			if olm == "y" {
-				installOLM()
-			}
 
 			ingress := promptLine("Install ingress-nginx", "[y,n]", "y")
 			if ingress == "y" {
@@ -67,12 +64,6 @@ func checkAllBinaries() {
 		cfmt.Println("{{✓ kubectl is installed}}::green")
 	}
 
-	if !checkBinary("operator-sdk") {
-		cfmt.Println("{{✗ operator-sdk is not installed}}::red")
-	} else {
-		cfmt.Println("{{✓ operator-sdk is installed}}::green")
-	}
-
 	if !checkBinary("kind") {
 		cfmt.Println("{{⚠ kind is not installed}}::yellow (only required if you want to install a local kind cluster)")
 	} else {
@@ -86,8 +77,12 @@ func checkBinary(binary string) bool {
 }
 
 func installKind() {
+	kindInstall := promptLine("Start a local kubernetes kind cluster", "[y,n]", "n")
+	if kindInstall != "y" {
+		return
+	}
 
-	if !checkBinary("kindooo") {
+	if !checkBinary("kind") {
 		log.Fatal("kind is not installed")
 	}
 
@@ -102,12 +97,12 @@ func installKind() {
 	fmt.Println("-------------- kind.yaml ---------------")
 	fmt.Println(kindConfig.String())
 	fmt.Println("----------------------------------------")
-	/*
-		kindConfigErr := os.WriteFile("kind.yaml", kindConfig.Body(), 0644)
-		if kindConfigErr != nil {
-			fmt.Println(kindConfigErr)
-			return
-		}
+	/* SKIPED for development
+	kindConfigErr := os.WriteFile("kind.yaml", kindConfig.Body(), 0644)
+	if kindConfigErr != nil {
+		fmt.Println(kindConfigErr)
+		return
+	}
 	*/
 
 	kindSpinner := spinner.New("Spin up a local Kind cluster")
@@ -120,18 +115,124 @@ func installKind() {
 	kindSpinner.Success("Kind cluster started sucessfully")
 
 	fmt.Println(string(out))
+}
+
+func checkCluster() {
+	var outb, errb bytes.Buffer
+
+	clusterInfo := exec.Command("kubectl", "cluster-info")
+	clusterInfo.Stdout = &outb
+	clusterInfo.Stderr = &errb
+	err := clusterInfo.Run()
+	if err != nil {
+		fmt.Println(errb.String())
+		fmt.Println(outb.String())
+		log.Fatal("command failed : kubectl cluster-info")
+	}
+	fmt.Println(outb.String())
+
+	out, _ := exec.Command("kubectl", "config", "get-contexts").Output()
+	fmt.Println(string(out))
+
+	clusterselect := promptLine("Is the CURRENT cluster the one you wish to install Kubero?", "[y,n]", "y")
+	if clusterselect == "n" {
+		os.Exit(0)
+	}
+}
+
+func installOLM() {
+
+	openshiftInstalled, _ := exec.Command("kubectl", "get", "deployment", "olm-operator", "-n", "openshift-operator-lifecycle-manager").Output()
+	if len(openshiftInstalled) > 0 {
+		cfmt.Println("{{✓ OLM is allredy installed}}::green")
+		return
+	}
+
+	//namespace := promptLine("Install OLM in which namespace?", "[openshift-operator-lifecycle-manager,olm]", "olm")
+	namespace := "olm"
+	olmInstalled, _ := exec.Command("kubectl", "get", "deployment", "olm-operator", "-n", namespace).Output()
+	if len(olmInstalled) > 0 {
+		cfmt.Println("{{✓ OLM is allredy installed}}::green")
+		return
+	}
+
+	olmInstall := promptLine("Install OLM", "[y,n]", "y")
+	if olmInstall != "y" {
+		log.Fatal("OLM is required to install Kubero")
+	}
+
+	olmRelease := promptLine("Install OLM from which release?", "[0.19.0,0.20.0,0.21.0,0.22.0]", "0.22.0")
+	olmURL := "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v" + olmRelease
+
+	olmCRDInstalled, _ := exec.Command("kubectl", "get", "crd", "subscriptions.operators.coreos.com").Output()
+	if len(olmCRDInstalled) > 0 {
+		cfmt.Println("{{✓ OLM CRD's allredy installed}}::green")
+	} else {
+		fmt.Println("run command : kubectl create -f " + olmURL + "/crds.yaml")
+		_, olmCRDErr := exec.Command("kubectl", "create", "-f", olmURL+"/crds.yaml").Output()
+		if olmCRDErr != nil {
+			cfmt.Println("{{✗ OLM CRD installation failed }}::red")
+			log.Fatal(olmCRDErr)
+		} else {
+			cfmt.Println("{{✓ OLM CRDs installed}}::green")
+		}
+	}
+
+	olmSpinner := spinner.New("Install OLM")
+	olmSpinner.Start("run command : kubectl create -f " + olmURL + "/olm.yaml")
+
+	_, olmOLMErr := exec.Command("kubectl", "create", "-f", olmURL+"/olm.yaml").Output()
+	if olmOLMErr != nil {
+		fmt.Println("")
+		olmSpinner.Error("Failed to run command. Try runnig it manually")
+		log.Fatal(olmOLMErr)
+	}
+	olmSpinner.Success("OLM installed sucessfully")
+
+	olmWaitSpinner := spinner.New("Wait for OLM to be ready")
+	olmWaitSpinner.Start("run command : kubectl wait --for=condition=available deployment/olm-operator -n " + namespace + " --timeout=60s")
+	_, olmWaitErr := exec.Command("kubectl", "wait", "--for=condition=available", "deployment/olm-operator", "-n", namespace, "--timeout=60s").Output()
+	if olmWaitErr != nil {
+		olmWaitSpinner.Error("Failed to run command. Try runnig it manually")
+		log.Fatal(olmWaitErr)
+	}
+	olmWaitSpinner.Success("OLM is ready")
+
+	olmWaitCatalogSpinner := spinner.New("Wait for OLM Catalog to be ready")
+	olmWaitCatalogSpinner.Start("run command : kubectl wait --for=condition=available deployment/catalog-operator -n " + namespace + " --timeout=60s")
+	_, olmWaitCatalogErr := exec.Command("kubectl", "wait", "--for=condition=available", "deployment/catalog-operator", "-n", namespace, "--timeout=60s").Output()
+	if olmWaitCatalogErr != nil {
+		olmWaitCatalogSpinner.Error("Failed to run command. Try runnig it manually")
+		log.Fatal(olmWaitCatalogErr)
+	}
+	olmWaitCatalogSpinner.Success("OLM Catalog is ready")
+}
+
+func installIngress() {
+
+	ingressInstalled, _ := exec.Command("kubectl", "api-resources", "--api-group=networking.k8s.io", "--namespaced=false", "--no-headers=true").Output()
+	if len(ingressInstalled) > 0 {
+		cfmt.Println("{{✓ Ingress is allredy installed}}::green")
+		return
+	}
+
+	ingressInstall := promptLine("Install Ingress", "[y,n]", "y")
+	if ingressInstall != "y" {
+		log.Fatal("Ingress is required to install Kubero")
+	} else {
+		ingressSpinner := spinner.New("Install Ingress")
+		ingressSpinner.Start("run command : kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.41.2/deploy/static/provider/cloud/deploy.yaml")
+		_, ingressErr := exec.Command("kubectl", "apply", "-f", "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.41.2/deploy/static/provider/cloud/deploy.yaml").Output()
+		if ingressErr != nil {
+			ingressSpinner.Error("Failed to run command. Try runnig it manually")
+			log.Fatal(ingressErr)
+		}
+		ingressSpinner.Success("Ingress installed sucessfully")
+	}
 
 }
 
 /*
-func installOLM() {
-	fmt.Println("install OLM")
-}
-
-func installIngressNginx() {
-	fmt.Println("install ingress-nginx")
-}
-
 func installKuberoOperator() {
 	fmt.Println("install kubero-operator")
 }
