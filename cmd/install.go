a79e2905824f29ff23763b82db420aad1183acd2
Gianni Carafa <gianni.carafa@srf.ch>
2022-11-22 12:50:12 +0100

    add GKE with gcloud binaries, since kubectl export requires a gcloud anyway

--- cmd/install.go
+++ cmd/install.go
@@ -77,6 +77,12 @@ func checkAllBinaries() {
 	} else {
 		cfmt.Println("{{✓ kind is installed}}::lightGreen")
 	}
+
+	if !checkBinary("gcloud") {
+		cfmt.Println("{{⚠ gcloud is not installed}}::yellow (only required if you want to install a GKE cluster)")
+	} else {
+		cfmt.Println("{{✓ gcloud is installed}}::lightGreen")
+	}
 }
 
 func checkBinary(binary string) bool {
@@ -90,7 +96,7 @@ func installSwitch() {
 		return
 	}
 
-	clusterType := promptLine("Select a cluster type", "[digitalocean,kind]", "digitalocean")
+	clusterType := promptLine("Select a cluster type", "[gke,digitalocean,kind]", "gke")
 	if clusterType == "kind" {
 		installKind()
 	}
@@ -104,16 +110,40 @@ func installSwitch() {
 }
 
 func installGKE() {
-	// TODO
-	// variables:
-	// - cluster name
-	// - cluster region
 	// gcloud config list
 	// gcloud config get project
+	// gcloud container clusters create kubero-cluster-4 --region=us-central1-c
 	// gcloud container clusters get-credentials kubero-cluster-4 --region=us-central1-c
 
 	// https://cloud.google.com/kubernetes-engine/docs/reference/libraries#client-libraries-install-go
 	// https://github.com/googleapis/google-cloud-go
+
+	gcloudName := promptLine("Kubernetes Cluster Name", "", "kubero-"+strconv.Itoa(rand.Intn(1000)))
+	gcloudRegion := promptLine("Region", "[https://cloud.google.com/compute/docs/regions-zones]", "us-central1-c")
+	gcloudClusterVersion := promptLine("Cluster Version", "[https://cloud.google.com/kubernetes-engine/docs/release-notes-regular]", "1.23.8-gke.1900")
+
+	spinner := spinner.New("Spin up a GKE cluster")
+	spinner.Start("run command : gcloud container clusters create " + gcloudName + " --region=" + gcloudRegion + " --cluster-version=" + gcloudClusterVersion)
+	_, err := exec.Command("gcloud", "container", "clusters", "create", gcloudName,
+		"--region="+gcloudRegion,
+		"--cluster-version="+gcloudClusterVersion).Output()
+	if err != nil {
+		fmt.Println()
+		spinner.Error("Failed to run command. Try runnig it manually and skip this step")
+		log.Fatal(err)
+	}
+	spinner.Success("GKE cluster started sucessfully")
+
+	spinner.Start("Get credentials for the GKE cluster")
+	_, err = exec.Command("gcloud", "container", "clusters", "get-credentials", gcloudName, "--region="+gcloudRegion).Output()
+	if err != nil {
+		fmt.Println()
+		spinner.Error("Failed to run command. Try runnig it manually and skip this step")
+		log.Fatal(err)
+	} else {
+		spinner.Success("GKE cluster credentials set")
+	}
+
 }
 
 func installDigitalOcean() {
