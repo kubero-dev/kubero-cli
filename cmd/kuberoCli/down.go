1da74729925e068cac650554d361181b9d576c46
Gianni Carafa <gianni.carafa@srf.ch>
2023-08-27 21:49:29 +0200

    [Refactor] Refactor down.go to improve undeploy functionality and add support for retrieving all local pipelines.

--- cmd/kuberoCli/down.go
+++ cmd/kuberoCli/down.go
@@ -1,6 +1,9 @@
 package kuberoCli
 
 import (
+	"log"
+	"os"
+
 	"github.com/i582/cfmt/cmd/cfmt"
 	"github.com/spf13/cobra"
 )
@@ -14,7 +17,7 @@ Subcommands:
   kubero down [pipeline|app]`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if pipelineName != "" && appName == "" {
-			downPipeline()
+			downPipeline(pipelineName)
 		} else if appName != "" {
 			downApp()
 		} else {
@@ -26,13 +29,20 @@ Subcommands:
 func init() {
 	rootCmd.AddCommand(downCmd)
 	downCmd.Flags().StringVarP(&pipelineName, "pipeline", "p", "", "name of the pipeline")
+	//downCmd.MarkFlagRequired("pipeline")
 	downCmd.Flags().StringVarP(&appName, "app", "a", "", "name of the app")
 }
 
-func downPipeline() {
+func downPipeline(pipelineName string) {
 	confirmation := promptLine("Are you sure you want to undeploy the pipeline "+pipelineName+"?", "[y,n]", "y")
 	if confirmation == "y" {
 		cfmt.Println("{{Undeploying pipeline}} " + pipelineName + "::yellow")
+
+		_, err := client.Delete("/api/cli/pipelines/" + pipelineName)
+		if err != nil {
+			panic("Unable to undeploy Pipeline")
+		}
+
 	} else {
 		cfmt.Println("{{Aborted}}::red")
 		return
@@ -46,9 +56,20 @@ func downApp() {
 		return
 	}
 
-	confirmation := promptLine("Are you sure you want to undeploy the app "+appName+" from "+pipelineName+"?", "[y,n]", "y")
+	if stageName == "" {
+		cfmt.Println("{{Please specify a stage}}::red")
+		return
+	}
+
+	confirmation := promptLine("Are you sure you want to undeploy the app "+appName+" from "+stageName+" in "+pipelineName+"?", "[y,n]", "y")
 	if confirmation == "y" {
 		cfmt.Println("{{Undeploying app}} " + appName + "::yellow")
+
+		_, err := client.Delete("/api/cli/pipelines/" + pipelineName + "/" + stageName + "/" + appName)
+		if err != nil {
+			panic("Unable to undeploy App")
+		}
+
 	} else {
 		cfmt.Println("{{Aborted}}::red")
 		return
@@ -59,8 +80,32 @@ func downAllPipelines() {
 	confirmation := promptLine("Are you sure you want to undeploy all pipelines?", "[y,n]", "n")
 	if confirmation == "y" {
 		cfmt.Println("{{Undeploying all pipelines}}::yellow")
+		getAllLocalPipelines()
 	} else {
 		cfmt.Println("{{Aborted}}::red")
 		return
 	}
+}
+
+func getAllLocalPipelines() []string {
+
+	basePath := "/.kubero/"
+	gitdir := getGitdir()
+	dir := gitdir + basePath + pipelineName
+
+	pipelineNames := []string{}
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			if _, err := os.Stat(dir + "/" + f.Name() + "/pipeline.yaml"); err == nil {
+				pipelineNames = append(pipelineNames, f.Name())
+			}
+		}
+	}
+
+	return pipelineNames
 }
